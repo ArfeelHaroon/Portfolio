0291bb5427255a459c7da73db53fc4ed570923f9
Arfeel Haroon <arfeelharoon178@gmail.com>
2025-06-28 11:31:49 -0400

    add the rel attribute to img tag for security

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,13 +5,13 @@ import { FaLinkedin } from 'react-icons/fa';
 function SocialLinks(){
     return(
      <div className="icons d-flex mt-auto">
-        <a href='https://github.com/ArfeelHaroon' target='_blank'>
+        <a href='https://github.com/ArfeelHaroon' target='_blank' rel="noopener noreferrer">
           <span className='py-3'><FaGithub size={30} color='#000'/></span>
         </a>
-        <a href="mailto:haroon.dev.web@gmail.com" target='_blank'>
+        <a href="mailto:haroon.dev.web@gmail.com" target='_blank' rel="noopener noreferrer">
           <span className='py-3'> <MdEmail size={30} color='#000'/></span>
         </a>
-        <a href='https://www.linkedin.com/in/arfeel-haroon-aa502a351' target='_blank'> 
+        <a href='https://www.linkedin.com/in/arfeel-haroon-aa502a351' target='_blank' rel="noopener noreferrer"> 
           <span className='py-3'><FaLinkedin size={30} color='#000'/></span>
         </a>
         </div>
