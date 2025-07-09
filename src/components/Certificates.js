83b434a1337100e28dc89b65ef6551e00c906d23
Arfeel Haroon <arfeelharoon178@gmail.com>
2025-07-09 10:22:57 -0400

    set the structure of certificates

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -15,23 +15,62 @@ function Certificates() {
                 <PiCertificateDuotone size={40} />
                 <span className="mx-3"> CERTIFICATES </span>
             </h3>
-            <div className="w-75 mx-auto mt-5">
+            <div className="certificatesContainer mx-auto mt-5">
                 <div className="container text-center mt-4">
                     <div className="align-items-center">
-                        <div className="row">
-                            <img alt="HTML Certificate" src={HTMLImg} className='certificate_imgs'/>
-                            <img alt="CSS Certificate" src={CSSImg} className='certificate_imgs'/> 
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                                <a href="https://www.coursera.org/account/accomplishments/certificate/1ONTZ8WJZS9G"
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    <img alt="HTML Certificate" src={HTMLImg} className="certificate-img" />
+                                </a>
+                            </div>
+                            <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                                <a href="https://www.coursera.org/account/accomplishments/certificate/KRGDFNFC202Y"
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    <img alt="CSS Certificate" src={CSSImg} className="certificate-img" />
+                                </a>
+                            </div>
                         </div>
-                        <div className="row">
-                            <img alt="JS Certificate" src={JSImg} className='certificate_imgs'/>
-                            <img alt="ResturantSite Certificate" src={resturantImg} className='certificate_imgs'/> 
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                                <a href="https://www.coursera.org/account/accomplishments/certificate/1X8BP7FI6SHE"
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    <img alt="JS Certificate" src={JSImg} className="certificate-img" />
+                                </a>
+                            </div>
+                            <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                                <a href="https://www.coursera.org/account/accomplishments/certificate/MJ8X7SVQNA9O" 
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    <img alt="ResturantSite Certificate" src={resturantImg} className="certificate-img" />
+                                </a>
+                            </div>
                         </div>
-                        <div className="row">
-                            <img alt="React Certificate" src={reactImg} className='certificate_imgs'/>
-                            {/* <img src={resturantImg} className='certificate_imgs'/>  */}
+                        <div className="row justify-content-center">
+                            <div className="col-12 col-md-6 mb-4 d-flex justify-content-center">
+                                <a href="https://www.coursera.org/account/accomplishments/certificate/14ZZC5BQ9ZCF"
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    <img alt="React Certificate" src={reactImg} className="certificate-img" />
+                                </a>
+                            </div>
                         </div>
 
-                      
+                        {/* <div className="row">
+                            <a className="w-50" href="https://www.coursera.org/account/accomplishments/certificate/1X8BP7FI6SHE" target="_blank" rel="noopener noreferrer">
+                                <img alt="JS Certificate" src={JSImg} className='certificate_imgs' />
+                            </a>
+                            <a href="https://www.coursera.org/account/accomplishments/certificate/MJ8X7SVQNA9O" target="_blank" rel="noopener noreferrer">
+                                <img alt="ResturantSite Certificate" src={resturantImg} className='certificate_imgs' />
+                            </a>
+                        </div> */}
+                        
+
+
                     </div>
                 </div>
 
