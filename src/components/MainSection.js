016c0656b396936795b7f10bfc5510f0cab222d0
Arfeel Haroon <arfeelharoon178@gmail.com>
2025-07-19 10:30:30 -0400

    add the bg to the initial

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -5,11 +5,11 @@ function MainSection() {
   return (
     <div className="main-container d-flex h-100 mt-4">
       <div className="left-section ">
-        <div className="logo mb-4">AH</div>
+        <div className="logo mb-4"><span className="bg-dark text-white py-2 px-1 fs-6 rounded-circle">AH</span></div>
         <div className="intro my-auto">
           <p className="greeting">Hi, I am</p>
           <h1 className="name">Arfeel Haroon</h1>
-          <p className="role">(Front-end / Back-end) Developer</p>
+          <p className="fw-bold">(Front-end / Back-end) Developer</p>
         </div>
         <SocialLinks />
       </div>
