72f462b9b92ba755b278024e0987f975e105ae46
ArfeelHaroon <129212947+ArfeelHaroon@users.noreply.github.com>
2025-12-03 19:12:25 -0500

    Update NavLinks.js
    add /Portfolio in nav links

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 function NavLinks(){
   return(
      <Nav className="ms-sm-1 ms-md-auto me-4">
-            <Nav.Link href="/#about">About me</Nav.Link>
-            <Nav.Link href="/#skills">Skills</Nav.Link>
-            <Nav.Link href="/#certificates">Certificates</Nav.Link>
+            <Nav.Link href="/Portfolio/#about">About me</Nav.Link>
+            <Nav.Link href="/Portfolio/#skills">Skills</Nav.Link>
+            <Nav.Link href="/Portfolio/#certificates">Certificates</Nav.Link>
             <Link to='/contact' className='d-block contact_btn fw-bold cursor-pointer text-center text-decoration-none py-2 px-2 mt-sm-2 mt-md-0 ms-4 mb-sm-2 mb-md-0'>Contact Me</Link>
     </Nav>
   );
